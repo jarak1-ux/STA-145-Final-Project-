80d52c66a6cacfb57a5eeb096133a98c402c9ddb
jarak1-ux <jarak1@tcnj.edu>
2025-12-02 19:16:59 -0500

    Create STA145-JARA.R

---
+++ STA145-JARA.R
@@ -0,0 +1,18 @@
+##  Project:  STA 145, Fall 2025, Final Project
+# Located:    Kline TCNJ Google Drive
+# File Name:  template
+# Date:       2025_11_25
+# Who:        Zachary D. Kline
+
+## Load packages
+# NOTE: Run base.R if these commands return an error!
+library(readr)
+library(dplyr)
+library(tidytext)
+library(tidyverse)
+library(ggplot2)
+library(haven)
+library(forcats)
+library(psych)
+
+# Load data
